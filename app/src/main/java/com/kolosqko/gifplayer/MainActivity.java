372a36940387825329d53f8e1279cc8c2639abe0
kolos <kolosqko@gmail.com>
2016-05-12 13:46:42 +0300

    Creating tabs

--- app/src/main/java/com/kolosqko/gifplayer/MainActivity.java
+++ app/src/main/java/com/kolosqko/gifplayer/MainActivity.java
@@ -1,16 +1,35 @@
+
 package com.kolosqko.gifplayer;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.TabHost;
+        import android.widget.TabHost.OnTabChangeListener;
+        import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+
+        tabHost.setup();
+        TabHost.TabSpec tabSpec;
+
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("Favorites");
+        tabSpec.setContent(R.id.tvTab1);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator("Add New Gif");
+        tabSpec.setContent(R.id.tvTab2);
+        tabHost.addTab(tabSpec);
+
+
+
     }
 }
